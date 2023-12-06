2ffbe8a1ad1f42dec0b8aeee1c41ff5a22b5d90c
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-06 13:18:49 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = ReactDom.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
